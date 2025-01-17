bd568be96ca1b6187794fee2bb5f9d7b844783b8
Andres Rengifo <andrew.rengifo.m@gmail.com>
2025-01-17 15:17:32 +0000

    Stage1 Passed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ nltk.download('punkt_tab')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 import string
+import re
 
 def extract_text_pypdf2(file_path):
     reader = PdfReader(file_path)
@@ -19,22 +20,25 @@ text = extract_text_pypdf2(pdf_file)
 def tokenize_text(text):
     words = nltk.word_tokenize(text)
     words = [word for word in words if word not in string.punctuation]
-    words = [word for word in words if word.isalnum()]
     sentences = nltk.sent_tokenize(text)
     return words, sentences
 
 words, sentences = tokenize_text(text)
 
-def word_frequency_analysis(words):
-    frequency = Counter(words)
-    return frequency.most_common(10)
-
-common_words = word_frequency_analysis(words)
-
 def filter_stopwords(words):
     stop_words = set(stopwords.words('english'))
     filtered_words = [word for word in words if word.lower() not in stop_words]
+    filtered_words = [re.sub(r"[^\w]", "", word) for word in filtered_words if re.sub(r"[^\w]", "", word)]
     return filtered_words
 
 filtered_words = filter_stopwords(words)
+
+def word_frequency_analysis(words):
+    frequency = Counter(words)
+    return frequency.most_common(50)    
+
+common_words = word_frequency_analysis(filtered_words)
+
 print("Filtered Words:", filtered_words)
+print("Common Words:", common_words)
+
